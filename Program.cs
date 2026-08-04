7ddfb4cc30525613de6ab89d7f1cdac027f89a1a
Gabriel <gabrielaxesm@gmail.com>
2026-08-04 11:32:55 -0300

    feat: manage to get every line of the file separatedly

--- Program.cs
+++ Program.cs
@@ -5,7 +5,16 @@ var file = new FileInfo("./TestFiles/add/Add.asm");
 
 using (StreamReader streamReader = new StreamReader(file.FullName, Encoding.UTF8))
 {
-  readContents = streamReader.ReadToEnd();
+  while (!streamReader.EndOfStream) 
+  {
+    // readContents = streamReader.ReadToEnd();
+    var line = streamReader.ReadLine();
+    Console.WriteLine($"{line}~");
+  }
 }
 
-Console.WriteLine(readContents);
+// foreach (var line in readContents)
+// {
+//   Console.WriteLine($"{line} \n");
+// }
+// Console.WriteLine(readContents);
