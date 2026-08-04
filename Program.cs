7c2bdb4538d2e94af30ff4891e041461c4abb737
Gabriel <gabrielaxesm@gmail.com>
2026-08-04 11:29:02 -0300

    refactor: read files in a more efficient manner

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿var file = new FileInfo("./TestFiles/add/Add.asm");
-Console.WriteLine(file.FullName);
-var lines = File.ReadAllText(file.FullName);
-Console.WriteLine(lines);
+﻿using System.Text;
+
+string readContents;
+var file = new FileInfo("./TestFiles/add/Add.asm");
+
+using (StreamReader streamReader = new StreamReader(file.FullName, Encoding.UTF8))
+{
+  readContents = streamReader.ReadToEnd();
+}
+
+Console.WriteLine(readContents);
