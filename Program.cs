10cd4abc4d1befb48611e43b8936a1d99d049b75
Gabriel <gabrielaxesm@gmail.com>
2026-08-09 16:33:03 -0300

    feat: convert A instruction to binary

--- Program.cs
+++ Program.cs
@@ -16,15 +16,26 @@ using (StreamReader streamReader = new StreamReader(file.FullName, Encoding.UTF8
 
 void interpret(string line) {
   if (!line.Any() || line.StartsWith("//")) return;
+  if (line.StartsWith("@"))
+  {
+    int address = Int32.Parse(line.Substring(1));
+    Print(Convert.ToString(address, 2).PadLeft(16, '0'));
+  }
   if (DEBUG)
   {
-    Print(line);
+    Print($": {line}");
   }
+  NewLine();
 }
 
 void Print(object? value)
 {
-  Console.WriteLine(value);
+  Console.Write(value);
+}
+
+void NewLine()
+{
+  Console.WriteLine();
 }
 
 // foreach (var line in readContents)
